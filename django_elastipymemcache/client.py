439b4676f57e5d3f13ae1175119af7392506979e
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2017-11-01 16:29:52 +0900

    Fix ignore pymemcache's get_many returns False value
    In some case, pymemcache returns {'key': False}
    if client does not found.
    
    It raises exception at Django-Cachalot.
    
    Django-Cachalot's author said get_many should not contain a key
    that's not in the cache, as stated in Django.
    
    So we try to imprements get_many method and ignore if False
    were included.

--- django_elastipymemcache/client.py
+++ django_elastipymemcache/client.py
@@ -2,4 +2,10 @@ from pymemcache.client.hash import HashClient
 
 
 class Client(HashClient):
-    pass
+    def get_many(self, keys, gets=False, *args, **kwargs):
+        # pymemcache's HashClient may returns {'key': False}
+        end = super(Client, self).get_many(keys, gets, args, kwargs)
+
+        return {key: end[key] for key in end if end[key]}
+
+    get_multi = get_many
